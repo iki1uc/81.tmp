7d4d75b447a11ccb5cb9a210fe32dcfb30d23498
iki1uc <entertrainer@web.de>
2026-07-27 16:23:51 +0200

    Add OrbitSync function to export atomic orbits

--- JS/OrbitSync.js
+++ JS/OrbitSync.js
@@ -1,1 +1,9 @@
+export function OrbitSync(atom) {
+    return {
+        O1: "Orbit(X) → " + atom.Elektron,
+        O2: "Orbit(Q) → " + atom.Proton,
+        O3: "Orbit(N) → " + atom.Neutron,
+        Dynamic: "Orbit-Dynamic → " + atom.Orbit
+    };
+}
 
