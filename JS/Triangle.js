4cdae351d0b99d5f3c3f15cfdadff7b421b6711a
iki1uc <entertrainer@web.de>
2026-07-27 16:24:21 +0200

    Create Triangle.js

---
+++ JS/Triangle.js
@@ -0,0 +1,9 @@
+export function Triangle(atom) {
+    return `
+        ▲ Dreieck 3×81
+        | Elektron: ${atom.Elektron}
+        | Proton:   ${atom.Proton}
+        | Neutron:  ${atom.Neutron}
+        ▼
+    `;
+}
