592c175e5794526c2136e05ed3b58e663d31a185
iki1uc <entertrainer@web.de>
2026-07-27 17:14:01 +0200

    Update 3×81.js

--- JS/3×81.js
+++ JS/3×81.js
@@ -1,15 +1,9 @@
-export async function Kernel81() {
-    const X = await fetch("../x.tmp").then(r => r.text());
-    const Q = await fetch("../q.tmp").then(r => r.text());
-    const OP = await fetch("../operator.tmp").then(r => r.text());
-    const ORBIT = await fetch("../U.orbit").then(r => r.text());
-    const N81 = await fetch("../81.tmp").then(r => r.text());
+export async function Kernel3x81() {
+    const Elektron = await fetch("./x.tmp").then(r => r.text());
+    const Proton   = await fetch("./q.tmp").then(r => r.text());
+    const Operator = await fetch("./operator.tmp").then(r => r.text());
+    const Orbit    = await fetch("./U/81.tmp").then(r => r.text());
+    const Neutron  = await fetch("./U/81.tmp").then(r => r.text());
 
-    return {
-        Elektron: X,
-        Proton: Q,
-        Neutron: N81,
-        Operator: OP,
-        Orbit: ORBIT
-    };
+    return { Elektron, Proton, Neutron, Operator, Orbit };
 }
