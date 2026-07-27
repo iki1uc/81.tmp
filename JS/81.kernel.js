9a54bd9325af762224e1ea6b673c440e12f03caa
iki1uc <entertrainer@web.de>
2026-07-27 16:23:07 +0200

    Update 81.kernel.js

--- JS/81.kernel.js
+++ JS/81.kernel.js
@@ -1,1 +1,16 @@
+export async function Kernel81() {
+    const X = await fetch("../x.tmp").then(r => r.text());
+    const Q = await fetch("../q.tmp").then(r => r.text());
+    const OP = await fetch("../operator.tmp").then(r => r.text());
+    const ORBIT = await fetch("../U.orbit").then(r => r.text());
+    const N81 = await fetch("../81.tmp").then(r => r.text());
+
+    return {
+        Elektron: X,
+        Proton: Q,
+        Neutron: N81,
+        Operator: OP,
+        Orbit: ORBIT
+    };
+}
 
