6a2a3994b4134114d70fddd29672a85e6c6c3a86
iki1uc <entertrainer@web.de>
2026-07-28 15:42:06 +0200

    Create DA.js

---
+++ JS/DA.js
@@ -0,0 +1,16 @@
+div.innerHTML = `
+REAL · REASPO · SCAN
+REAL (Echtzeit)
+${JSON.stringify(structure.X, null, 4)}
+REASPO (Response)
+${JSON.stringify(structure.Q, null, 4)}
+SCAN (Analyse)
+${JSON.stringify(structure.TMP, null, 4)}
+`;
+REAL · REASPO · SCAN
+REAL (Echtzeit)
+{ … }
+REASPO (Response)
+{ … }
+SCAN (Analyse)
+{ … }
