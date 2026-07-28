31c3941f3bc35fc6b8ccb61f38078c7acd47965b
iki1uc <entertrainer@web.de>
2026-07-28 15:54:25 +0200

    Update NE.js

--- JS/NE.js
+++ JS/NE.js
@@ -1,24 +1,7 @@
-export function REAL_REASPO_SCAN() {
-
-    const div = document.createElement("div");
-    div.style.background = "#111";
-    div.style.color = "#eee";
-    div.style.padding = "20px";
-    div.style.margin = "20px 0";
-    div.style.border = "1px solid #333";
-
-    div.innerHTML = `
-        <h2>REAL · REASPO · SCAN</h2>
-
-        <h3>REAL (Echtzeit)</h3>
-        <pre>${JSON.stringify(structure.X, null, 4)}</pre>
-
-        <h3>REASPO (Response)</h3>
-        <pre>${JSON.stringify(structure.Q, null, 4)}</pre>
-
-        <h3>SCAN (Analyse)</h3>
-        <pre>${JSON.stringify(structure.TMP, null, 4)}</pre>
-    `;
-
-    return div;
-}
+export const REAL_DATA = {
+    active: true,
+    cpu: 0.63,
+    mem: 0.41,
+    route: "stable",
+    tmp: 0.22
+};
