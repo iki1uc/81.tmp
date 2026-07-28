4e3d0e30f382ac7d6324956774808cc9824788cc
iki1uc <entertrainer@web.de>
2026-07-28 16:48:31 +0200

    Update 81.viewer.js

--- JS/81.viewer.js
+++ JS/81.viewer.js
@@ -1,1 +1,20 @@
 
+// tmp/81.viewer.js
+
+import { MATRIX_81 } from "./81.tmp.js";
+
+export function VIEW_81_MATRIX() {
+
+    const div = document.createElement("div");
+    div.className = "block";
+
+    div.innerHTML = `
+        <h2>WSAD‑Matrix</h2>
+        W: ${MATRIX_81.W}<br>
+        S: ${MATRIX_81.S}<br>
+        A: ${MATRIX_81.A}<br>
+        D: ${MATRIX_81.D}<br>
+    `;
+
+    return div;
+}
