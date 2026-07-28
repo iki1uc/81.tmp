2230a2c10161259b290284fdfe99e7935b2d9c59
iki1uc <entertrainer@web.de>
2026-07-28 20:00:03 +0200

    Update 81tmp.hyper.js

--- JS/81tmp.hyper.js
+++ JS/81tmp.hyper.js
@@ -1,1 +1,22 @@
+// 81tmp.hyper.js
+// Hyper‑Matrix für 81.tmp
+
+import { MATRIX_81 } from "./81.tmp.js";
+
+export function HYPER_81() {
+
+    const hyper = {};
+
+    for (const key in MATRIX_81) {
+        const val = MATRIX_81[key];
+        hyper[key] = {
+            raw: val,
+            sq: val ** 2,
+            rt: Math.sqrt(Math.abs(val)),
+            sign: Math.sign(val)
+        };
+    }
+
+    return hyper;
+}
 
