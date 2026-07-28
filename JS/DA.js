ae97704fd074a1f9870bf10cb3fe12a4f1404fe5
iki1uc <entertrainer@web.de>
2026-07-28 16:07:41 +0200

    Add files via upload

---
+++ JS/DA.js
@@ -0,0 +1,7 @@
+export const HDF_DATA = {
+    status: "ok",
+    load: 0.82,
+    integrity: 0.94,
+    flux: 0.12,
+    error: false
+};
---
+++ JS/NEU.js
@@ -0,0 +1,11 @@
+// JS/NEU.js
+
+export function NEU() {
+    return {
+        status: "ok",
+        level: 1,
+        flux: 0.22,
+        active: true,
+        scoreBase: 0.5
+    };
+}
