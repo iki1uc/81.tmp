26e959db769b23dea4589dd8070dfa0d04552b33
iki1uc <entertrainer@web.de>
2026-07-28 16:20:50 +0200

    Delete tmp/Triangle.js

--- tmp/Triangle.js
+++
@@ -1,9 +0,0 @@
-export function Triangle(atom) {
-    return `
-        ▲ Dreieck 3×81
-        | Elektron: ${atom.Elektron}
-        | Proton:   ${atom.Proton}
-        | Neutron:  ${atom.Neutron}
-        ▼
-    `;
-}
