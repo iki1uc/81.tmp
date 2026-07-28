69180d6c835249def7e98a8ee9519f76ed457623
iki1uc <entertrainer@web.de>
2026-07-28 16:12:56 +0200

    Create 81.js

---
+++ JS/81.js
@@ -0,0 +1,8 @@
+import { WSAD } from "./wsad.vector.js";
+
+const vektor = WSAD(atom);
+
+UPDATE_81(MATRIX_81, "d1", vektor.W);
+UPDATE_81(MATRIX_81, "d2", vektor.S);
+UPDATE_81(MATRIX_81, "d3", vektor.A);
+UPDATE_81(MATRIX_81, "d4", vektor.D);
